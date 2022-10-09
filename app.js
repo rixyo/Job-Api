25f114d4cdee2eaa1f457c51b2cc521348b15c77
rixyo <saleh35-3063@diu.edu.bd>
2022-10-09 18:41:43 +0600

    bug fixed

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ const port = process.env.PORT || 3000;
 const start = async () => {
     try {
       await connectDB(process.env.MONGO_URI);
-      console.log(process.env.MONGO_URI);
+      //console.log(process.env.MONGO_URI);
       app.listen(port, () =>
         console.log(`Server is listening on port ${port}...`)
       );
